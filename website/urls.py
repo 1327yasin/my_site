76b99a09413213996f5ff459b6e8f6d25b61d650
1327yasin <yasinnikzad92@gmail.com>
2026-09-15 16:49:37 +0330

    created the first element

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from website.views import * 
+
+urlpatterns = [
+    path('', home_page),
+    path('contact', contact),
+    path('about', about_us),
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_page(request):
+    return HttpResponse("<h1>home page</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>contact</h1>")
+
+def about_us(request):
+    return HttpResponse ("<h1>abot us</h1>")
